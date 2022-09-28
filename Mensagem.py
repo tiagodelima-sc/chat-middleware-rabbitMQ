77455f31bb4984583c8d70ce94b8147eedabddba
Tiago de Lima <tiago.l07@aluno.ifsc.edu.br>
2022-09-28 10:10:39 -0300

    att interface

--- Mensagem.py
+++ Mensagem.py
@@ -2,35 +2,36 @@ import time
 from threading import Thread
 try:
     from tkinter import *
-    from tkinter import messagebox
+    from tkinter import messagebox, simpledialog
 except:
     input('Parece que o Tkinter não está instalado na sua maquina. Por favor, instale a lib para continuar a execução.')
     exit()
-
 import pika
 
 PRIVADO = False
 EMGRUPO = False
-REMETENTE = input('Remetente: ')
 control = True
+REMETENTE = simpledialog.askstring(title="Remetente", prompt="Digite o Remetente:")
+
+# Controle para definir se a entrega das mensagens é Privado ou em Grupo
 
 while control:
-
-    opt = int(input('[1] - Privado \t [2] - Grupo\n '))
+    opt = simpledialog.askinteger(title="Grupo", prompt="[1] - Privado [2] - Grupo")
     if opt == 1:
-        DESTINATARIO = input('Destinatario: ')
+        DESTINATARIO = simpledialog.askstring(title="Destinatario", prompt="Digite o Destinatário:")
         PRIVADO = True
         control = False
     else:
         if opt == 2:
-            GRUPO = input('Grupo: ')
+            GRUPO = simpledialog.askstring(title="Grupo", prompt="Digite o Nome do Grupo:")
             EMGRUPO = True
             control = False
         else:
-            print('ERRO! Digite uma opção válida.\n')
+            messagebox.showinfo("ERRO!", "Digite uma opção válida")
+
+# Função para receber as mensagens (Privado e em Grupo).
 
 def receiver():
-
     def chamada(ch, method, propreties, body):
         if opt == 1:
             lista_mensagem.insert(END, f"{DESTINATARIO} disse: " + body.decode('utf-8'))
@@ -64,7 +65,7 @@ def receiver():
     channel.start_consuming()
     connection.close()
 
-
+# Função para enviar as mensagens (Privado e em Grupo).
 
 def send():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -78,37 +79,30 @@ def send():
     if PRIVADO:
         channel.queue_declare(queue=DESTINATARIO)
         live = entry_field.get()
-        lista_mensagem.insert(END, f"Voce falou: " + live)
+        lista_mensagem.insert(END, f"Você falou: " + live)
         channel.basic_publish(exchange='', routing_key=DESTINATARIO, body= live.encode('utf-8'))
 
     connection.close()
 
-
-# Iniciando layout
+# Iniciando o layout
 
 tela = Tk()
 tela.title(f'Chat Ao Vivo - {REMETENTE}')
 tela.geometry("500x325+300+100")
 tela.configure(bg = "#474440")
-
-# Definindo se pode aumentar ou diminuir
 tela.resizable(False, False)
 
-# icon
-# tela.iconbitmap("img/icon-whats.ico")
-
-# Funcao e Botao Sair
+# Função para sair da Conversa
 
 def sair():
     lista_mensagem.delete(0,END)
     tela.destroy()
     messagebox.showinfo("Aviso", f"{REMETENTE} Deslogou")
 
-
 botao_sair = Button(tela, text = "Sair Agora", command= sair, fg="#fff" , bg="#FF2511")
 botao_sair.pack(side = TOP, anchor = NE, pady = 5, padx = 5)
 
-# Apresenta as mensagens na tela
+# Mensagens na tela através do layout
 
 messages_frame = Frame(tela)
 scrollbar = Scrollbar(messages_frame)
@@ -118,7 +112,7 @@ lista_mensagem.pack(side=LEFT, fill=BOTH)
 lista_mensagem.pack()
 messages_frame.pack()
 
-# Vai Receber os valores que o usuario digitar com a caixa de texto
+# Recebe os valores que o usuario digitar com a caixa de texto
 
 botao_frame = Frame(tela)
 botao_frame.configure(bg="#474440")
@@ -127,14 +121,17 @@ lb.pack(side = LEFT, anchor= S, pady = 15, padx=5)
 entry_field = Entry(botao_frame, textvariable = '', bg = "#fff")
 entry_field.pack(side = LEFT, anchor = SE,pady = 15)
 
-# Botao enviar a mensagem
+# Botão para chamar a função "send" e enviar a mensagem
 botao_enviar = Button(botao_frame, text= "Enviar", command=send,fg="#fff", bg = "#32CD30")
 botao_enviar.pack(side = LEFT, anchor = S, pady = 15, padx = 5)
 
 botao_frame.pack()
 
 receber = Thread(target= receiver)
-#mandar = Thread(target= send)
 receber.start()
-#mandar.start()
+
 tela.mainloop()
+
+
+
+
